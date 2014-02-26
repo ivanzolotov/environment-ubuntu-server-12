6404ec8bc1d484f8483d931337d0120c79df60b6
ivanzolotov <ivanzolotov@ivanzolotov.ru>
2014-02-26 21:10:51 +0600

    add user-dependency condition

--- 2-user.sh
+++ 2-user.sh
@@ -1,9 +1,16 @@
 # DigitalOcean and Netangels only!
 # Not for AWS!
-useradd -m -d /home/ubuntu ubuntu
-mkdir /home/ubuntu/.ssh
-cp /root/.ssh/authorized_keys /home/ubuntu/.ssh
-chown -R ubuntu:ubuntu /home/ubuntu/.ssh
-echo "ubuntu ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ubuntu
-chmod 440 /etc/sudoers.d/ubuntu
-usermod -s /bin/bash ubuntu
+if (whoami = root) then
+	(useradd -m -d /home/ubuntu ubuntu
+	mkdir /home/ubuntu/.ssh
+	cp /root/.ssh/authorized_keys /home/ubuntu/.ssh
+	chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+	echo "ubuntu ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ubuntu
+	chmod 440 /etc/sudoers.d/ubuntu
+	usermod -s /bin/bash ubuntu
+	# Restrict root login via SSH
+	sudo sed -i -e 's/PermitRootLogin\ yes/PermitRootLogin\ no/g' /etc/ssh/sshd_config)
+else
+	echo 1
+fi
+
